8230d34476d8eb14d92b652d2d7fa5370b2900b3
Michael R <mpcooleo@gmail.com>
2018-10-12 12:03:50 -0500

    Hardware Map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -10,14 +10,17 @@ public class TestTeleOp extends OpMode {
 
     private PIDControl driveTrainPID;
     private double targetXPower, targetYPower;
-    private DcMotor leftRear, rightRear, leftFront, rightFront;
+    private DcMotor rearLeft, rearRight, frontLeft, frontRight;
 
     @Override
     public void init() {
         driveTrainPID = new PIDControl(0,0,0);
         targetXPower = 0;
         targetYPower = 0;
-        //Comment
+        rearLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        rearRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
     }
 
     @Override
@@ -30,20 +33,20 @@ public class TestTeleOp extends OpMode {
 
     /*
      * Moves the Robot in only the four cardinal directions, depending on which controller axis is being pushed the furthest.
-     * Goes forward, backward, left, and right.
+     * Goes forward, backward, left, or right.
      */
     //Untested
     public void fourDirectionalMovement() {
         if(Math.abs(targetXPower) > Math.abs(targetYPower)) {
-            rightRear.setPower(targetXPower);
-            leftRear.setPower(-targetXPower);
-            rightFront.setPower(-targetXPower);
-            leftFront.setPower(targetXPower);
+            rearRight.setPower(targetXPower);
+            rearLeft.setPower(-targetXPower);
+            frontRight.setPower(-targetXPower);
+            frontLeft.setPower(targetXPower);
         } else {
-            leftFront.setPower(targetYPower);
-            rightFront.setPower(targetYPower);
-            leftRear.setPower(targetYPower);
-            rightRear.setPower(targetYPower);
+            frontLeft.setPower(targetYPower);
+            frontRight.setPower(targetYPower);
+            rearLeft.setPower(targetYPower);
+            rearRight.setPower(targetYPower);
         }
     }
 
@@ -53,10 +56,10 @@ public class TestTeleOp extends OpMode {
      */
     //Untested
     public void omniDirectionalMovement() {
-        rightRear.setPower(targetXPower + targetYPower);
-        rightFront.setPower(-targetXPower + targetYPower);
-        leftRear.setPower(-targetXPower + targetYPower);
-        leftFront.setPower(targetXPower + targetYPower);
+        rearRight.setPower(targetXPower + targetYPower);
+        frontRight.setPower(-targetXPower + targetYPower);
+        rearLeft.setPower(-targetXPower + targetYPower);
+        frontLeft.setPower(targetXPower + targetYPower);
     }
 
 }
